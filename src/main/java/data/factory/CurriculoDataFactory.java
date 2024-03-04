0450c1107237fe25bc5b90a91ae043510ba28eec
Raphael Baere de Sá Oliveira <raphaelbaere@id.uff.br>
2024-03-04 19:26:06 -0300

    fix: path do curriculo

--- src/main/java/data/factory/CurriculoDataFactory.java
+++ src/main/java/data/factory/CurriculoDataFactory.java
@@ -5,10 +5,10 @@ import java.io.File;
 public class CurriculoDataFactory {
 
     public static File curriculo1() {
-        return new File("C:\\Users\\pedro\\Documents\\dev\\chronos-qa-api\\src\\main\\resources\\curriculo\\Currículo.docx");
+        return new File("C:\\Users\\rapha\\projeto-final-api\\chronos-qa-api\\src\\main\\resources\\curriculo\\Currículo.docx");
     }
     public static File curriculo2() {
-        return new File("C:\\Users\\pedro\\Documents\\dev\\chronos-qa-api\\src\\main\\resources\\curriculo\\Currículo3.docx");
+        return new File("C:\\Users\\rapha\\projeto-final-api\\chronos-qa-api\\src\\main\\resources\\curriculo\\Currículo3.docx");
     }
 
 }
