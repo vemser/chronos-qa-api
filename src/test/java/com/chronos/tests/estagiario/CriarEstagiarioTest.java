414435e4f980dc10fbcc1aed7b833bff2849eef9
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2024-03-04 20:59:31 -0300

    fix: asserção de telefone na criação de estagiário

--- src/test/java/com/chronos/tests/estagiario/CriarEstagiarioTest.java
+++ src/test/java/com/chronos/tests/estagiario/CriarEstagiarioTest.java
@@ -20,6 +20,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("ESTAGIÁRIO: CRIAÇÃO")
@@ -113,7 +114,7 @@ public class CriarEstagiarioTest {
                 .contentType(ContentType.JSON)
                 .statusCode(HttpStatus.SC_BAD_REQUEST)
                 .body("status", equalTo(HttpStatus.SC_BAD_REQUEST))
-                .body("errors[0]", equalTo(message));
+                .body("errors", hasItem(message));
     }
 
     @Feature("Estagiario")
